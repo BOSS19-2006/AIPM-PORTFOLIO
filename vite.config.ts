51a09268665d1858c5c14b3f65d6ba7148a3f794
Atharv Chaure <123456789+BOSS19-2006@users.noreply.github.com>
2026-09-22 19:05:46 +0530

    Configure Vite for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite';
+﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/AIPM-PORTFOLIO/',
   plugins: [react()],
   resolve: {
     alias: {
